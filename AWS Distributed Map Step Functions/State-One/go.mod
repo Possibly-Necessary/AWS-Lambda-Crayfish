ca17ab36d42e72a673b73dfb28d795f10025c33d
Zahraa A. AlAbdullah <109365947+Possibly-Necessary@users.noreply.github.com>
2024-08-12 18:44:15 +0300

    Add files via upload

---
+++ AWS Distributed Map Step Functions/State-One/go.mod
@@ -0,0 +1,5 @@
+module step-one
+
+go 1.21.1
+
+require github.com/aws/aws-lambda-go v1.47.0
---
+++ AWS Distributed Map Step Functions/State-One/initialize-divide.go
@@ -0,0 +1,101 @@
+// Lambda function that intialized sub-populations and trigger Step Function state machine
+
+package main
+
+import (
+	"context"
+	"math/rand"
+	benchmark "step-one/benchmark"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+// Structure for the input event of the state machine
+type crayfishParameters struct {
+	N int    `json:"n"`
+	K int    `json:"k"`
+	T int    `json:"t"`
+	F string `json:"f"`
+}
+
+// Structure for the output event for the processing lambda function (within the distributed map)
+type SubPopulation struct {
+	SubPop [][]float64 `json:"subpop"` // Subpopulation i
+	T      int         `json:"t"`
+	F      string      `json:"f"`
+}
+
+type PopulationData struct {
+	SubPopulations []SubPopulation `json:"subPopulations"`
+}
+
+// Funtion to initialize and divide the population
+func initializePopulation(N, k, t int, f string) PopulationData { // Instead of returning ([]byte, error)
+	// Set the timer
+	//startTime := time.Now()
+
+	// Get the benchmark function data
+	funcData := benchmark.GetFunction(f) // will hold the string name of the function e.g. "F6"
+	lb := funcData.LB                    // Get the upper/lower bound and dimension assocciated with the selected function
+	ub := funcData.UB
+	dim := funcData.Dim
+
+	// Initialize the population N x Dim matrix, X
+	X := make([][]float64, N)
+	for i := 0; i < N; i++ {
+		X[i] = make([]float64, dim)
+	}
+
+	for i := range X {
+		for j := range X[i] {
+			X[i][j] = rand.Float64()*(ub[0]-lb[0]) + lb[0]
+		}
+	}
+
+	// Split the population based on k
+	totalSize := len(X)
+	baseSubPopSize := totalSize / k // N/k
+	remainder := totalSize % k
+
+	//Xsub := make([][][]float64, k)
+	Xsub := make([]SubPopulation, k)
+	startIndex := 0
+	//subPopCount := 0
+
+	for i := 0; i < k; i++ {
+		subPopSize := baseSubPopSize
+		if remainder > 0 { // In case the division is not even
+			subPopSize++ // Add one of the remaining individuals to this sub-population
+			remainder--
+		}
+		//Xsub[i] = X[startIndex : startIndex+subPopSize]
+		//startIndex += subPopSize
+
+		Xsub[i] = SubPopulation{
+			SubPop: X[startIndex : startIndex+subPopSize],
+			T:      t,
+			F:      f,
+		}
+		startIndex += subPopSize
+	}
+
+	return PopulationData{SubPopulations: Xsub}
+
+}
+
+func HandleDivision(ctx context.Context, parameters crayfishParameters) (PopulationData, error) {
+
+	// These parameters are defined as an input in the State Machine
+	// Crayfish parameters: population, sub-populations, COA iteration
+	//N, k, t := 500, 20, 500
+	//F := Benchmark function
+
+	result := initializePopulation(parameters.N, parameters.K, parameters.T, parameters.F)
+
+	return result, nil
+
+}
+
+func main() {
+	lambda.Start(HandleDivision)
+}
